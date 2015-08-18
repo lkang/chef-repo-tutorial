0fca0e443ce9b6162712c0642ec900c3d3ddb3f9
larry kang <root@cheftest1>
2015-08-18 00:47:19 -0400

    update for newest sql

--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -8,34 +8,41 @@
 #
 
 include_recipe "apache2"
-include_recipe "mysql::client"
-include_recipe "mysql::server"
-#include_recipe "php5-fpm"
-#include_recipe "php"
-#include_recipe "php::module_mysql"
-#include_recipe "apache2::mod_php5"
-include_recipe "mysql::ruby"
+#include_recipe "mysql::client"
+#include_recipe "mysql::server"
+include_recipe "php5-fpm"
+include_recipe "php"
+include_recipe "php::module_mysql"
+include_recipe "apache2::mod_php5"
+#include_recipe "mysql::ruby"
 include_recipe "mysql2_chef_gem"
 
 apache_site "default" do
   enable true
 end
 
+mysql2_chef_gem 'default' do
+  action :install
+end
+
+mysql_service 'phpapp' do
+  
+  action: [:create, :start]
+end
+
+connection_info = {
+  :host => 'localhost',
+  :username => 'root',
+  :password => node['mysql']['server_root_password']
+}
+
 mysql_database node['phpapp']['database'] do
-  connection({
-    :host => 'localhost',
-    :username => 'root',
-    :password => node['mysql']['server_root_password']
-  })
+  connection(connection_info)
   action :create
 end
 
 mysql_database_user node['phpapp']['db_username'] do
-  connection({
-    :host => 'localhost',
-    :username => 'root',
-    :password => node['mysql']['server_root_password']
-  })
+  connection(connection_info)
 
   password node['phpapp']['db_password']
   database_name node['phpapp']['database']
